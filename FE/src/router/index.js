8231c98c085c57977216a7ac985018a0895d6336
dlgmldnd017 <dlgmldnd017@naver.com>
2024-01-22 17:12:46 +0900

    Test : test

--- FE/src/router/index.js
+++ FE/src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainView from '../views/Main.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
+    {
+      path: '/',
+      name: 'main',
+      component: MainView
+    },
   ]
 })
 
--- FE/src/views/Main.vue
+++ FE/src/views/Main.vue
@@ -1,13 +1,9 @@
 <template>
-  
 </template>
 
 <script>
-export default {
 
-}
 </script>
 
 <style>
-
 </style>
