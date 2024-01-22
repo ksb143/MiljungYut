062f900e7bc085a95b466d5457f00b8769311815
dlgmldnd017 <dlgmldnd017@naver.com>
2024-01-22 12:43:09 +0900

    Remove: MafiaClient Folder

--- FE/MafiaClient/js/itemFunction.js
+++
@@ -1,5 +0,0 @@
-CallBack["itemfunction.deaho"] = () => {
-    let audio = new Audio("./assets/Items/sounds/deahoCity.mp3");
-    audio.volume = 0.2;
-    audio.play();
-}
--- FE/MafiaClient/js/keymanager.js
+++
@@ -1,9 +0,0 @@
-let KeyManager = new Array();
-
-$(function() {
-    $(document).keydown(function(e) {
-        KeyManager.forEach(element => {
-            element(e.keyCode);
-        })
-    });
-});
