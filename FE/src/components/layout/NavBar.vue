06235c93fdb1e652970daa7d084dff88a6fa5549
양성규 <yt1356@naver.com>
2024-01-23 15:04:52 +0900

    Feat : navBar 생성

---
+++ FE/src/components/layout/NavBar.vue
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<template>
+  <div class="nav-bar">
+    <div>
+      <img src="../../assets/logo/logo.png" class="img-log" />
+    </div>
+    <div class="text-box">
+      <router-link to="/" class="nav-text">홈</router-link>
+      <router-link to="/" class="nav-text">게임</router-link>
+      <router-link to="/" class="nav-text">게시판</router-link>
+    </div>
+    <div class="pro-box">
+        <router-link to="/" class="nav-pro">프로필</router-link>
+    </div>
+  </div>
+</template>
+
+<style>
+@import "../../assets/css/layout/navBar.css";
+</style>
