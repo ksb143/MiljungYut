240b89ff30a245b2e4de95d82a71339dda64adb4
dlgmldnd017 <dlgmldnd017@naver.com>
2024-01-25 07:40:24 +0900

    Design: 홈이랑 사이드바 배치

--- FE/src/view/home/HomeView.vue
+++ FE/src/view/home/HomeView.vue
@@ -1,8 +1,33 @@
-<script>
-
-</script>
-
 <template>
-    <div>home
+    <div class="container">
+      <div class="event-container">
+       
+      </div>
+  
+      <div class="article-title">
+        <div id="patch-title">
+          <h2>패치노트</h2>
+          <button>더보기></button>
+        </div>
+        <div id="faq-title">
+          <h2>FAQ</h2>
+          <button>더보기></button>
+        </div>
+      </div>
+  
+      <div class="article-container">
+        <div id="patch">
+          <div class="patch-list"></div>
+        </div>
+        <div id="faq"></div>
+      </div>
     </div>
-</template>
+  </template>
+      
+  <script>
+  
+  </script>
+      
+  <style scoped>
+  @import url("../../assets/css/home/home.css");
+  </style>
