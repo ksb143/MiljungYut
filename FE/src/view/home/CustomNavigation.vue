30c59f4bd07cc117554e8d5380c727ce1f417a22
dlgmldnd017 <dlgmldnd017@naver.com>
2024-01-25 10:45:40 +0900

    Feat: 슬라이더 구현

---
+++ FE/src/view/home/CustomNavigation.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="custom-navigation">
+    <!-- 왼쪽 화살표 아이콘 (이전 슬라이드로 이동) -->
+    <div class="custom-arrow custom-arrow-left" @click="goToPrev">
+      <i class="fas fa-chevron-left"></i> <!-- 이 아이콘은 Font Awesome 라이브러리에서 가져옴 -->
+    </div>
+
+    <!-- 오른쪽 화살표 아이콘 (다음 슬라이드로 이동) -->
+    <div class="custom-arrow custom-arrow-right" @click="goToNext">
+      <i class="fas fa-chevron-right"></i> <!-- 이 아이콘은 Font Awesome 라이브러리에서 가져옴 -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToPrev() {
+      this.$emit("goToPrev");
+    },
+    goToNext() {
+      this.$emit("goToNext");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.custom-navigation {
+  display: flex;
+  align-items: center;
+}
+
+.custom-arrow {
+  cursor: pointer;
+  font-size: 24px;
+  color: white; /* 아이콘을 하얀색으로 설정 */
+  margin: 0 10px;
+}
+
+.custom-arrow:hover {
+  /* 호버 상태일 때 스타일 변경 가능 */
+  color: #007bff; /* 원하는 호버 색상으로 변경 */
+}
+</style>
--- FE/src/view/home/HomeView.vue
+++ FE/src/view/home/HomeView.vue
@@ -1,33 +1,95 @@
 <template>
-    <div class="container">
-      <div class="event-container">
-       
+  <div class="container">
+    <!-- (상단) 이벤트 슬라이더 -->
+    <div class="event-container">
+      <Carousel :autoplay="3000" :wrap-around="true">
+        <Slide v-for="slide in 6" :key="slide">
+          <!-- <div class="carousel__item">{{ i18n[slide].name }}</div> -->
+          <div class="carousel__item">
+            <img id="gallery" :src="i18n[slide].name" />
+          </div>
+          <CustomNavigation @goToPrev="goToPrev" @goToNext="goToNext" />
+        </Slide>
+
+        <template #addons>
+          <Pagination />
+        </template>
+      </Carousel>
+    </div>
+
+    <!-- (중간) 아티클 제목 -->
+    <div class="article-title">
+      <!-- 패치노트 -->
+      <div id="patch-title">
+        <h2>패치노트</h2>
+        <button id="detail-btn">더보기></button>
       </div>
-  
-      <div class="article-title">
-        <div id="patch-title">
-          <h2>패치노트</h2>
-          <button>더보기></button>
-        </div>
-        <div id="faq-title">
-          <h2>FAQ</h2>
-          <button>더보기></button>
-        </div>
-      </div>
-  
-      <div class="article-container">
-        <div id="patch">
-          <div class="patch-list"></div>
-        </div>
-        <div id="faq"></div>
+
+      <!-- FAQ -->
+      <div id="faq-title">
+        <h2>도움말</h2>
+        <button id="detail-btn">더보기></button>
       </div>
     </div>
-  </template>
+
+    <!-- (하단) 아티클 내용 -->
+    <div class="article-container">
+      <!-- 패치노트 -->
+      <div id="patch">
+        <div class="patch-list"></div>
+      </div>
+
+      <!-- FAQ -->
+      <div id="faq"></div>
+    </div>
+  </div>
+</template>
       
   <script>
-  
-  </script>
+import { defineComponent, ref } from "vue";
+import { Carousel, Slide } from "vue3-carousel";
+import CustomNavigation from "./CustomNavigation.vue"; // 커스텀 Navigation 컴포넌트
+
+import "vue3-carousel/dist/carousel.css";
+
+import test01 from "../../assets/img/home/event01.png";
+import test02 from "../../assets/img/home/test02.png";
+import test03 from "../../assets/img/home/test03.png";
+import test04 from "../../assets/img/home/test04.png";
+import test05 from "../../assets/img/home/test05.png";
+import test06 from "../../assets/img/home/test06.png";
+
+export default defineComponent({
+  name: "Autoplay",
+  components: {
+    Carousel,
+    Slide,
+    CustomNavigation,
+  },
+
+  methods: {
+    goToPrev() {
+      this.$refs.carousel.prev();
+    },
+    goToNext() {
+      this.$refs.carousel.next();
+    },
+  },
+
+  data: () => ({
+    i18n: [
+      {},
+      { name: test01 },
+      { name: test02 },
+      { name: test03 },
+      { name: test04 },
+      { name: test05 },
+      { name: test06 },
+    ],
+  }),
+});
+</script>
       
   <style scoped>
-  @import url("../../assets/css/home/home.css");
-  </style>
+@import url("../../assets/css/home/home.css");
+</style>
