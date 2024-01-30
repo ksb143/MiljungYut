2f3271892d714994d14568bc604f99f7142a23d5
dlgmldnd017 <dlgmldnd017@naver.com>
2024-01-30 10:02:40 +0900

    Fix: 피니아 -> 세션

--- FE/src/components/layout/NavPro.vue
+++ FE/src/components/layout/NavPro.vue
@@ -2,35 +2,42 @@
   <div class="nav-pro">
     <!-- 사용자가 설정한 사진을 보여준다 -->
     <div class="nav-img">
-      <img src="../../assets/icon/user.png" class="img" />
+      <img src="../../assets/icon/user.png" @click="goToUserProfile" class="img" />
     </div>
     <!-- 사용자의 정보를 간략히 보여준다 -->
     <div class="nav-info">
       <div class="nav-name">
-        <span>{{ userName }}</span>
+        <span>{{ userName }}님</span>
       </div>
       <!-- 클릭 시 프로필로 화면을 이동한다 -->
       <div class="nav-my">
-        <router-link to="/user" class="nav-btn">내정보</router-link>
+        <router-link to="/" class="nav-btn">로그아웃</router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { useUserStore } from "@/store/userStore";
-
 export default {
+  
   data() {
     return {};
   },
 
+  methods: {
+    goToUserProfile() {
+      // 사용자 프로필 페이지로 이동
+      this.$router.push('/user');
+    },
+  },
+
   computed: {
     userName() {
-      const userInfo = useUserStore().userInfo;
+      const userInfo = JSON.parse(sessionStorage.getItem("userInfo"));
       return userInfo ? userInfo.name : "";
     },
   },
+
 };
 </script>
 
