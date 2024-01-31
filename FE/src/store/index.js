0c05b84397528bc77c1a48311511ad778e3cc7be
김수빈 <ksb143@naver.com>
2024-01-31 17:05:34 +0900

    Desing: 배경화면 맵 마다 변경

--- FE/src/store/index.js
+++ FE/src/store/index.js
@@ -1,7 +1,7 @@
 // pinia아 생성
 import { createPinia } from 'pinia';
 //pinia storage 저장 plugin
-import { piniaPersist } from 'pinia-plugin-persist';
+import piniaPersist from 'pinia-plugin-persist';
 
 export const useStore = createPinia();
 useStore.use(piniaPersist);
