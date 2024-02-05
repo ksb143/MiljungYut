24841e0f8066b9c1af2d9f50d7d53ab0c461c558
김수빈 <ksb143@naver.com>
2024-02-05 17:56:47 +0900

    Feat: 파리 로직 수정
    Change-Id: Ic22cfac8c7bb32f4b00b383892936edcbf1b6d40

--- FE/src/components/mission/FlyCatch.vue
+++ FE/src/components/mission/FlyCatch.vue
@@ -1,16 +1,7 @@
 <template>
     <!-- 라이브 웹캠 화면을 표시하는 컨테이너 -->
     <div id="liveView" class="videoView">
-      <!-- 웹캠 활성화/비활성화 버튼 -->
-      <button
-        id="webcamButton"
-        class="mdc-button mdc-button--raised"
-        @click="toggleWebcam"
-      >
-        <span class="mdc-button__ripple"></span>
-        <span class="mdc-button__label">{{ webcamButtonText }}</span>
-      </button>
-      <div style="position: relative;">
+      <div class="media-container" style="position: relative;">
         <!-- 웹캠 비디오 요소 -->
         <video
           class="media"
@@ -18,30 +9,27 @@
           ref="webcam"
           autoplay
           playsinline
-          :width="videoWidth"
-          :height="videoHeight"
         ></video>
-        <!-- 웹캠 비디오 위에 손동작 그릴 캔버스 -->
-        <canvas
-          class="media"
-          id="output_canvas"
-          ref="output_canvas"
-          style="position: absolute; left: 0px; top: 0px;"
-        ></canvas>
         <!-- 웹캠 비디오 위에 파리 그릴 캔버스 -->
         <canvas
           class="media"
-          id="fly_canvas"
+          id="fly-canvas"
           ref="fly_canvas"
-          style="position: absolute; left: 0px; top: 0px;"
+          style="position: absolute; left: auto; top: 0px;"
         ></canvas>
-        <!-- 제스처 결과를 표시할 요소 -->
-        <div id="gesture_output" class="output" v-if="gestureOutput">
-          <p>카테고리 - 왼손: {{ leftCategoryName }} / 오른손: {{ rightCategoryName }}</p>
-          <p>손인식 정확도 - 왼손: {{ leftCategoryScore }} / 오른손: {{ rightCategoryScore }}</p>
-          <p>x축 - 왼손: {{ leftXAxis }} / 오른손: {{ rightXAxis }}</p>
-          <p>y축 - 왼손: {{ leftYAxis }} / 오른손: {{ rightYAxis }}</p>
-        </div>
+      </div>
+      <!-- 게임 타이머 -->
+      <div class="game-timer" v-if="gestureOutput">
+        {{ gameTimer }}
+      </div>
+      <!-- 시작 전 타이머 -->
+      <div class="start-timer" v-else>
+        {{ countdown }}
+      </div>
+      <!-- 게임 승리 -->
+      <div class="victory" v-if="gameResultDp">
+        <div v-if="gameResult">게임승리</div>
+        <div v-else>게임패배</div>
       </div>
     </div>
 </template>
@@ -57,16 +45,12 @@
         // 웹 캠 관련
         gestureRocognizer: GestureRecognizer,
         runningMode: "VEDIO", // 웹캠 실행 모드
-        webcamButtonText: "웹 캠 활성화", // 웹캠 활성화 버튼 텍스트
+        webcamButtonText: "제스처 인식 로드 전", // 웹캠 활성화 버튼 텍스트
         webcamRunning: false, // 웹캠 실행 여부
         lastVideoTime: -1, // 비디오의 마지막 시간
         constraints: {
           video: true
         },
-
-        // 비디오 크기
-        videoWidth: 480,
-        videoHeight: 360,
 
         // 제스쳐 인식 결과
         results: undefined,
@@ -88,14 +72,40 @@
         flyImg: new Image(),
         flyPosition: { x: Math.random(), y: Math.random() },
         flyDirection: { x: (Math.random() - 0.5) * 50, y: (Math.random() - 0.5) * 50 },
+        flies: [],
+        numOfFlies: 3,
+
+        // 게임 카운트
+        countdown: 5,
+        gameTimer: 10,
+
+        // 게임 결과
+        gameResultDp: false,
+        gameResult: false,
       }
     },
     
     mounted() {
       this.createGestureRecognizer(); // 제스처 인식기 초기화
+      this.startCountdown(); // 게임 시작 카운트
+      window.addEventListener('resize', this.adjustCanvasSizeToVideo)
+    },
+
+    beforeDestroy() {
+      window.removeEventListener('resize', this.adjustCanvasSizeToVideo)
     },
 
     methods: {
+      // 캔버스 크기를 비디오 크기에 맞게 조정
+      adjustCanvasSizeToVideo() {
+        const videoElement = this.$refs.webcam
+        const flyCanvasElement = this.$refs.fly_canvas
+        if(videoElement && flyCanvasElement) {
+          flyCanvasElement.width = videoElement.offsetWidth
+          flyCanvasElement.height = videoElement.offsetHeight
+        }
+      },
+
       // 제스처 인식기 초기화 및 캔버스 크기 설정
       async createGestureRecognizer() {
         const vision = await FilesetResolver.forVisionTasks(
@@ -115,60 +125,59 @@
         );
         const videoElement = this.$refs.webcam;
         videoElement.addEventListener('loadedmetadata', () => {
-          // 비디오 크기에 맞게 캔버스 크기 설정
-          this.videoWidth = videoElement.videoWidth;
-          this.videoHeight = videoElement.videoHeight;
-          this.adjustCanvasSize();
+          // 로드되면 비디오 사이즈에 맞추어 캔버스 사이즈 맞추기
+          this.adjustCanvasSizeToVideo()
         })
       },
 
-      
-      // 캔버스 요소들에 비디오 크기 적용
-      adjustCanvasSize() {
-        const outputCanvas = this.$refs.output_canvas;
-        const flyCanvas = this.$refs.fly_canvas
-        if (outputCanvas && flyCanvas) {
-          outputCanvas.width = this.videoWidth
-          outputCanvas.height = this.videoHeight
-          flyCanvas.width = this.videoWidth
-          flyCanvas.height = this.videoHeight
-        }
+
+      // 카운트다운 시작
+      startCountdown() {
+        const countdownInterval = setInterval(() => {
+          this.countdown -= 1
+          if (this.countdown === 0) {
+            clearInterval(countdownInterval)
+            this.startGameTimer();
+          }
+        }, 1000)
       },
 
 
-      // 유저 웹 캠 접근 여부 확인
-      hasGetUserMedia() {
-        return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+      // 게임시작
+      startGameTimer() {
+        this.toggleWebcam()
+        const gameInterval = setInterval(() => {
+          this.gameTimer -= 1
+          if (this.gameTimer === 0) {
+            clearInterval(gameInterval)
+            this.webcamRunning = false
+            this.clearFlyCanvas()
+            this.gameResultDp = true
+          }
+        }, 1000)
       },
-      
+
 
       // 웹 캠 토글
-      toggleWebcam() {
+      async toggleWebcam() {
         // 제스처 인식기 초기화 덜 됐을 경우
         if (!this.gestureRecognizer) {
           alert("제스처 인식기가 로드되길 기다려주세요");
           return;
         }
-        if (this.webcamRunning) {
-          // 웹 캠 비활성화
-          this.webcamRunning = false;
-          this.gestureOutput = false
-          this.webcamButtonText = "제스처 인식 시작하기";
-          // 로드된 파리 캔버스 클리어
-          // this.clearFlyCanvas();
-        } else {
-          // 웹 캠 활성화
-          this.webcamRunning = true;
-          this.gestureOutput = true
-          this.webcamButtonText = "제스처 인식 끝내기";
-
-          navigator.mediaDevices.getUserMedia(this.constraints).then((stream) => {
-            const videoElement = this.$refs.webcam;
-            videoElement.srcObject = stream;
-            videoElement.addEventListener("loadeddata", this.predictWebcam);
-          });
-          // 파리 이미지 로드 및 애니메이션 시작 로직 유지
-          this.loadAndAnimateFly();
+        // 웹 캠 활성화
+        this.webcamRunning = true;
+        this.gestureOutput = true
+        this.webcamButtonText = "제스처 인식 로드 완료";
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia(this.constraints)
+          const videoElement = this.$refs.webcam;
+          videoElement.srcObject = stream
+          videoElement.addEventListener("loadeddata", this.predictWebcam)
+          this.initializeFlies();
+        } catch (error) {
+          alert("웹 캠에 접근할 수가 없습니다.")
+          console.log(error)
         }
       },
 
@@ -190,7 +199,7 @@
             const categoryName = gesture[0].categoryName
             const currentTime = Date.now()
             const timeDiff = currentTime - this.lastGetureTime
-            if (timeDiff < 500) {
+            if (timeDiff < 100) {
               this.gestureSequence.push(categoryName)
             } else {
               this.gestureSequence = [categoryName];
@@ -203,35 +212,10 @@
             console.log('파리 잡기 성공')
             this.webcamRunning = false
             this.clearFlyCanvas()
+            this.gameResult = true
+            this.gameResultDp = true
           }
         }
-        // // 손 제스쳐 그릴 준비 (확인용)
-        // const canvasElement = this.$refs.output_canvas;
-        // const canvasCtx = canvasElement.getContext("2d");
-        // // 현재 캔버스 상태를 저장
-        // canvasCtx.save();
-        // // 캔버스 내용 지우기
-        // canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height)
-        // const drawingUtils = new DrawingUtils(canvasCtx);
-        // // 제스처 랜드마크 그리기
-        // if (this.results.landmarks) {
-        //   for (const landmarks of this.results.landmarks) {
-        //     drawingUtils.drawConnectors(
-        //       landmarks,
-        //       GestureRecognizer.HAND_CONNECTIONS,
-        //       {
-        //         color: "#00FF00",
-        //         lineWidth: 5
-        //       }
-        //     );
-        //     drawingUtils.drawLandmarks(landmarks, {
-        //       color: "#FF0000",
-        //       lineWidth: 2
-        //     });
-        //   }
-        // }
-        // // 2D 컨텍스트에 대한 상태를 이전 상태로 복원
-        // canvasCtx.restore();
         // 인식된 손 정보
         if (this.results.gestures.length == 1) {
           this.leftCategoryName = this.results.gestures[0][0].categoryName
@@ -253,16 +237,17 @@
         // 손모양 계속 인식을 위해 계속 호출
         if (this.webcamRunning) {
           window.requestAnimationFrame(this.predictWebcam);
-        } else {
-          const outputCanvasElement = this.$refs.output_canvas;
-          const outputCanvasCtx = outputCanvasElement.getContext("2d");
-          outputCanvasCtx.clearRect(0, 0, outputCanvasElement.width, outputCanvasElement.height);
         }
       },
 
+      // 파리 초기화
+      initializeFlies() {
+        this.flies = Array.from({ length: this.numOfFlies }).map(() => ({
+          position: { x: Math.random(), y: Math.random() },
+          direction: { x: (Math.random() - 0.5) * 30, y: (Math.random() - 0.5) * 30 },
+          caught: false,
+        }))
 
-      // 파리 이미지 로드 및 애니메이션 시작
-      loadAndAnimateFly() {
         this.flyImg.src = flyImage;
         this.flyImg.onload = () => {
           console.log('이미지가 성공적으로 로드되었습니다.');
@@ -272,6 +257,18 @@
           console.log('이미지 로드 중에 오류가 발생했습니다.')
         }
       },
+
+      // // 파리 이미지 로드 및 애니메이션 시작
+      // loadAndAnimateFly() {
+      //   this.flyImg.src = flyImage;
+      //   this.flyImg.onload = () => {
+      //     console.log('이미지가 성공적으로 로드되었습니다.');
+      //     this.animateFly();
+      //   };
+      //   this.flyImg.onerror = () => {
+      //     console.log('이미지 로드 중에 오류가 발생했습니다.')
+      //   }
+      // },
       
 
       // 파리 이미지 그리기
@@ -284,6 +281,7 @@
           }
           const canvasCtx = canvasElement.getContext("2d");
           canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height) // 캔버스 클리어
+
           // 파리의 이동 방향에 따라 이미지 반전
           if (this.flyDirection.x < 0) {
             // 왼쪽으로 이동 중이면 이미지 반전
@@ -302,9 +300,9 @@
       // 파리 움직이는 로직
       animateFly() {
         if (this.webcamRunning) {
-          const canvasElement = this.$refs.fly_canvas;
-          const canvasWidth = canvasElement.width;
-          const canvasHeight = canvasElement.height;
+          const flyCanvas = this.$refs.fly_canvas
+          const canvasWidth = flyCanvas.width;
+          const canvasHeight = flyCanvas.height;
           // 파리의 새 위치 계산
           let newX = this.flyPosition.x + this.flyDirection.x;
           let newY = this.flyPosition.y + this.flyDirection.y;
@@ -328,11 +326,11 @@
 
       // 파리와 손의 위치가 가까운지 판단
       checkFlyCaught() {
-          const outputCanvas = this.$refs.output_canvas;
-          const leftHandX = this.leftXAxis * outputCanvas.width
-          const leftHandY = this.leftYAxis * outputCanvas.height
-          const rightHandX = this.rightXAxis * outputCanvas.width
-          const rightHandY = this.rightYAxis * outputCanvas.height
+          const vedioElement = this.$refs.webcam;
+          const leftHandX = this.leftXAxis * vedioElement.width
+          const leftHandY = this.leftYAxis * vedioElement.height
+          const rightHandX = this.rightXAxis * vedioElement.width
+          const rightHandY = this.rightYAxis * vedioElement.height
           const flyX = this.flyPosition.x
           const flyY = this.flyPosition.y
           const leftDistance = Math.sqrt(Math.pow(leftHandX - flyX, 2) + Math.pow(leftHandY - flyY, 2))
@@ -364,11 +362,5 @@
 </script>
 
 <style scoped>
-.media {
-  transform: rotateY(180deg);
-  -webkit-transform:rotateY(180deg); /* Safari and Chrome */
-}
-.output {
-  background-color: white;
-}
+@import url('@/assets/css/mission/flyCatch.css');
 </style>
