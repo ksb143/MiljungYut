ac59538578568e018b50cd866290f3a8fd9abf00
양성규 <yt1356@naver.com>
2024-02-05 11:15:38 +0900

    Feat : 게임 윷놀이 1차 완성
    Change-Id: I95142963a9120217b05f48c802d8c5609d03b2c1

--- FE/src/components/game/MiniGame.vue
+++ FE/src/components/game/MiniGame.vue
@@ -1,36 +1,20 @@
 <template>
   <div class="mini-main">
-    <div class="roulette-main">
-        <MiniRoulette/>
+    <div class="card-main">
+        <MiniCard/>
     </div>
   </div>
 </template>
 
 <script>
-import MiniRoulette from './item/MiniRoulette.vue'
+import MiniCard from './item/MiniCard.vue';
 export default {
     components : {
-        MiniRoulette,
+        MiniCard,
     }
 }
 </script>
 
 <style>
-.mini-main{
-    position: relative;
-    z-index: 1001;
-    width: 100%;
-    height: 100vh;
-    background-color: rgba(0, 0, 0, 0.5);
-}
-.roulette-main{
-    position: absolute;
-    width: 600px;
-    height: 600px;
-    top: 75px;
-    left: 450px;
-    background-color: rgb(0, 187, 187);
-    border-radius: 20px;
-    border: 5px solid white;
-}
+@import "@/assets/css/game/miniGame.css";
 </style>
