f698109fb4f571279a29f8dfce4fc652be28bddb
dlgmldnd017 <dlgmldnd017@naver.com>
2024-02-15 01:20:35 +0900

    Fix : 테스트용 애니메이션 삭제

--- FE/src/components/game/pick/WaitSpyPick.vue
+++ FE/src/components/game/pick/WaitSpyPick.vue
@@ -2,7 +2,10 @@
   <!-- 패치노트 모달창 -->
   <div class="modal">
     <div class="modal-content">
-      <h2 ref="titleElement" class="title">
+      <!-- <h2 ref="titleElement" class="title">
+        우리 팀 리더가 스파이 픽을 하고 있습니다...
+      </h2> -->
+      <h2 class="title">
         우리 팀 리더가 스파이 픽을 하고 있습니다...
       </h2>
     </div>
@@ -24,23 +27,23 @@ export default {
   },
   methods: {
     startLoadingAnimation() {
-      const dots = [".", "..", "...", "....", "....."];
-      let index = 0; 
-      this.intervalId = setInterval(() => {
-        this.loadingText = dots[index];
-        index = (index + 1) % dots.length; 
-        this.$refs.titleElement.innerText =
-          "우리 팀 리더가 스파이 픽을 하고 있습니다" + this.loadingText;
-      }, 800);
-      this.$emit("close-modal", "start");
+    //   const dots = [".", "..", "...", "....", "....."];
+    //   let index = 0; 
+    //   this.intervalId = setInterval(() => {
+    //     this.loadingText = dots[index];
+    //     index = (index + 1) % dots.length; 
+    //     this.$refs.titleElement.innerText =
+    //       "우리 팀 리더가 스파이 픽을 하고 있습니다" + this.loadingText;
+    //   }, 800);
+    //   this.$emit("close-modal", "start");
     },
   },
   mounted() {
-    this.startLoadingAnimation(); 
+    // this.startLoadingAnimation(); 
   },
   beforeDestroy() {
-    clearInterval(this.intervalId);
-    this.intervalId = null;
+    // clearInterval(this.intervalId);
+    // this.intervalId = null;
   },
 };
 </script>
