a3bc5a7f984eccea915e9f27864be3dbd0df80e5
김수빈 <ksb143@naver.com>
2024-02-16 03:42:17 +0900

    Fix: 방 검색 로딩 오류 수정

---
+++ FE/src/components/home/PatchDetail.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- 패치노트 모달창 -->
+  <div class="modal">
+    <div class="modal-content">
+      <font-awesome-icon :icon="['fas', 'x']" style="color: #ffffff;" class="close" @click="closeModal('patchDetail')" />
+      <h2 class="title">{{patchDetail.subject}}</h2>
+      <br />
+      <p>{{ patchDetail.content }}</p>
+      <br />
+    </div>
+  </div>
+</template>
+
+<script>
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+library.add(faX)
+
+export default {
+  props: {
+    patchDetail: Object
+  },
+
+  methods: {
+    closeModal(modalId) {
+      this.$emit("close-modal", modalId);
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import url("../../assets/css/home/patch.css")
+</style>
